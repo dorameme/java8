7dbf0a8a619ba3bbf32ee3952b7078b2a2ffd56d
dorameme <doradorav@naver.com>
2024-06-13 21:17:45 +0900

    람다 표현식

--- src/main/java/hello/java8/Foo.java
+++ src/main/java/hello/java8/Foo.java
@@ -1,21 +1,24 @@
 package hello.java8;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.IntConsumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 public class Foo {
 
     public static void main(String[] args) {
-        Plus10 plus10=new Plus10();//함수형 인터페이스 구현.
+        Plus10 plus10 = new Plus10();//함수형 인터페이스 구현.
         System.out.println(plus10.apply(10));
 
-        Function<Integer,Integer> plus1=(i)->i+1; //함수형 인터페이스 람다구현.
+        Function<Integer, Integer> plus1 = (i) -> i + 1; //함수형 인터페이스 람다구현.
         System.out.println(plus1.apply(10));
 
-        Function<Integer,Integer> multiply = i ->i*2;//함수형 인터페이스 람다구현.
+        Function<Integer, Integer> multiply = i -> i * 2;//함수형 인터페이스 람다구현.
 
         //default함수 compose를 이용하여 고차원 함수로 사용.
         Function<Integer, Integer> compose = plus1.compose(multiply);
@@ -27,13 +30,42 @@ public class Foo {
         Consumer<Integer> printT = System.out::println;
         printT.accept(10);
 
-        Supplier<Integer> supply= ()->10;
+        Supplier<Integer> supply = () -> 10;
         System.out.println(supply.get());
 
         Predicate<String> startWithKim = (s) -> s.startsWith("Kim");
         System.out.println(startWithKim.test("Kim"));
         //입력값과 결과값의 타입이 같은 경우
-        UnaryOperator<Integer> plus100= (i) -> i+100;
+        UnaryOperator<Integer> plus100 = (i) -> i + 100;
         System.out.println(plus100.apply(10));
+        //Integer + Integer = Integer;
+        BinaryOperator<Integer> sum = (a, b) -> a + b;
     }
+
+    private void run() {
+        final int baseNum = 10;
+        //local
+        class LocalClass {
+
+            int baseNum=11;
+
+            void printBaseNum() {
+                System.out.println(baseNum);
+            }
+        }
+        //익명클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            int baseNum=11;
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(baseNum);
+            }
+        };
+        //lambda -> 쉐도잉이 안된다...run 을 같은 스콥으로 들어감.
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNum);
+        };
+        printInt.accept(10);
+    }
+
 }
